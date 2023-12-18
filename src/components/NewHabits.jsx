a4c088cbe414772e0a7894681e33683eada1e553
AlexanderLindgren1 <Alexprestoxxx@gmail.com>
2023-12-18 09:37:29 +0100

    added code to newhabits and coded habits use the data from new habits

--- src/components/NewHabits.jsx
+++ src/components/NewHabits.jsx
@@ -1,3 +1,32 @@
 export function NewHabits(props) {
+    let list = []
+
+
+
+
+    function submit() {
+        
+        const title = document.getElementById("title").value
+        const streak = document.getElementById("streak").value
+
+        let object = {
+            title: { title },
+            givenumber: streak,
+        }
+
+
+       return list.push(object)
+
+  
+
+    }
+
+
+    console.log(list);
+    return <><input type="text" id="title" /> <input type="number" id="streak" /> <button onClick={() =>  submit() }>submit</button>
     
+    <br />
+    <ul>  {list && list.map((habit) =>{console.log(habit); return <li> {habit.title} </li>})} </ul>
+    </>
+
 }
