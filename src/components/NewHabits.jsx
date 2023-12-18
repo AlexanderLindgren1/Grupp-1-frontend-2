beda93a6e5745b394a252fbf7e80f180ad5fb021
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2023-12-18 10:19:15 +0100

    fixed code

--- src/components/NewHabits.jsx
+++ src/components/NewHabits.jsx
@@ -1,32 +1,35 @@
+import { useState } from "react"
+
 export function NewHabits(props) {
-    let list = []
+    let [movehabit, setmovehabit] = useState()
 
 
+       let list = []
 
 
     function submit() {
-        
+
         const title = document.getElementById("title").value
         const streak = document.getElementById("streak").value
 
         let object = {
-            title: { title },
-            givenumber: streak,
+            title: title,
+            givenumber: streak
         }
+ 
+        list.push(object)
+        
 
 
-       return list.push(object)
 
-  
 
     }
 
 
-    console.log(list);
-    return <><input type="text" id="title" /> <input type="number" id="streak" /> <button onClick={() =>  submit() }>submit</button>
-    
-    <br />
-    <ul>  {list && list.map((habit) =>{console.log(habit); return <li> {habit.title} </li>})} </ul>
+    return <><input type="text" id="title" /> <input type="number" id="streak" /> <button onClick={() => submit()}>submit</button>
+
+        {/* <br />
+        <ul>  {list.length > 0 && list.map((habit) => { console.log(habit); return <li> {habit.title} </li> })} </ul> */}
     </>
 
 }
