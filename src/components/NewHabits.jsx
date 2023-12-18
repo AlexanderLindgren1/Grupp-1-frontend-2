533638a01f1fd7a897c1339184fd8d3e670f013d
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2023-12-18 21:54:50 +0100

    transfer code to github

--- src/components/NewHabits.jsx
+++ src/components/NewHabits.jsx
@@ -2,53 +2,66 @@ import { useState } from "react"
 
 export function NewHabits(props) {
 
-    let [master, setmaster] = useState([])
+    let [habitList, setHabitList] = useState([])
+    let [moveobject, setMoveObject] = useState()
+    let index = 0
 
-    let clonemaster = [...master]
+    let clonemaster = [...habitList]
 
-    function submit(params) {
+    function addHabit(params) {
         const title = document.getElementById("title").value
         const streaks = document.getElementById("streaks").value
+
+
+        console.log(props.data);
         let object = {
             title: title,
-            streaks: streaks
+            streaks: props.data && props.data
+        }
+
+        clonemaster.push(object)
+        setHabitList(clonemaster)
+        console.log(habitList[0]);
+        if (habitList[1]) {
+            habitList[1].streaks = "Alexander"
         }
 
 
-        clonemaster.push(object)
-
-        console.log(clonemaster);
-        setmaster(clonemaster)
-        master.map((tt) => { console.log(tt); })
 
 
     }
+ 
+    function clx(dxx, index) {
+        console.log(index);
+        return habitList[index].streaks = props.data
 
-    let ranking = () => {
+
 
     }
 
 
     return (
-        <>
-           <input type="text" id="title" />
+        <div >
+            <input type="text" id="title" />
             <input type="number" id="streaks" />
-            <button onClick={() => submit()}>submit</button>
+            <button onClick={() => addHabit()}>submit</button>
+       
 
-            {master.map((tt, index) => { return <><p key={index}>title: {tt.title} streaks: {tt.streaks} </p> 
-          
-            <input type="radio" id="low" value={"low"} name="Prioritet " />
-            <label for="low">low</label>
+            {habitList.map((tt, index) => {
+                return <div key={index} className="item"><p>title: {tt.title} streaks: {tt.streaks}  <button onClick={() => clx(tt, index)}>clx </button></p>
 
-            <input type="radio" id="medium" value={"medium"} name="Prioritet " />
-            <label for="medium">medium</label>
+                    <input type="radio" id={"low" + index} value={"low"} name={"Prioritet "+ index} />
+                    <label for={"low"+ index}>low</label>
 
-            <input type="radio" id="heigest" value={"heigest"} name="Prioritet " />
-            <label for="heigest">heigest</label>
-        </>
-        })}
+                    <input type="radio" id={"medium"+ index} value={"medium"} name={"Prioritet "+ index} />
+                    <label for={"medium"+ index}>medium</label>
+
+                    <input type="radio" id={"heigest"+ index} value={"heigest" } name={"Prioritet "+ index} />
+                    <label for={"heigest"+ index}>heigest</label>
+                </div>
+            })}
 
 
-        </>)
+        </div>)
 
 }
