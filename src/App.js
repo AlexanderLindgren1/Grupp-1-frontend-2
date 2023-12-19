94c05f084139b6649e0edd0f3b387c9a7a52b96e
Ammar <ammari11alhalabi@gmail.com>
2023-12-19 15:10:50 +0100

    ammar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Habits.css';
+
 import { NewHabits } from './components/NewHabits';
+
+import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom';
+import { Habits } from './components/Habits';
+
 function App() {
   return (
- 
+
     <div className="App">
-    
-    
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Habits' element={<Habits />} />
+
+      </Routes>
 
     </div>
   );
