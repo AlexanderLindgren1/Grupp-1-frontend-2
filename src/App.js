3211ec8a3f64d0a04df3e23699f31b21328f47b5
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2023-12-20 11:34:22 +0100

    remove soon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { NewHabits } from './components/NewHabits';
+import { Habits } from './components/Habits';
+import './components/Habits.css';
+
 function App() {
   return (
- 
-    <div className="App">
-    
-    
 
-    </div>
+    <div className="App">
+
+
+    <Habits/>
+    </div >
   );
 }
 
