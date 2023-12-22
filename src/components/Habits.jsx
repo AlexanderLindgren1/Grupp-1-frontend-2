786dd8f45da8beb9437d46e99d9e98132363a67f
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2023-12-22 07:27:55 +0100

    useing chat gpt to ranking system

--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -1,44 +1,53 @@
 import { useState } from "react";
-
 import { NewHabits } from "./NewHabits";
 
 export function Habits(props) {
   let [changer, setchanger] = useState("adduktion");
-
+  let [sortingOption, setSortingOption] = useState("none");
   let [habits, sethabits] = useState([
     { Title: "kaka", streak: 4 },
     { Title: "Lama", streak: 8 },
   ]);
 
   function addhabits(objects) {
-    let upppdatedHabits = [...habits];
-    console.log(upppdatedHabits);
-    upppdatedHabits.push(objects);
-    sethabits(upppdatedHabits);
+    let updatedHabits = [...habits];
+    updatedHabits.push(objects);
+    sethabits(updatedHabits);
   }
+
   let updatehabit = (index) => {
+    let updatedHabits = [...habits];
 
-    
-    let updatehabits = [...habits];
-
-    if (changer == "adduktion") {
-      updatehabits[index].streak += 1;
-    } else if (changer == "Zero") {
-      updatehabits[index].streak = 0;
-    } else if (changer == "subtraction") {
-      updatehabits[index].streak -= 1;
+    if (changer === "adduktion") {
+      updatedHabits[index].streak += 1;
+    } else if (changer === "Zero") {
+      updatedHabits[index].streak = 0;
+    } else if (changer === "subtraction") {
+      updatedHabits[index].streak -= 1;
     }
 
-    sethabits(updatehabits);
+    sethabits(updatedHabits);
   };
 
-  let rankingHighToLow = (index) => {
-  console.log("kaka");
-    habits.sort((a, b) => {
-        console.log(habits);
-      return b["streak"] - a["streak"];
+  let handleSortingChange = (event) => {
+    setSortingOption(event.target.value);
+    if (event.target.value === "highest") {
+      rankingHighToLow();
+    } else if (event.target.value === "lowest") {
+      rankingLowToHigh();
+    }
+  };
 
-    });
+  let rankingHighToLow = () => {
+    let sortedHabits = [...habits];
+    sortedHabits.sort((a, b) => b.streak - a.streak);
+    sethabits(sortedHabits);
+  };
+
+  let rankingLowToHigh = () => {
+    let sortedHabits = [...habits];
+    sortedHabits.sort((a, b) => a.streak - b.streak);
+    sethabits(sortedHabits);
   };
 
   return (
@@ -46,22 +55,17 @@ export function Habits(props) {
       <h1>Habits</h1>
       <button onClick={() => setchanger("adduktion")}>+1</button>
       <button onClick={() => setchanger("Zero")}>0</button>
-
       <button onClick={() => setchanger("subtraction")}>-1</button>
 
-      <select name="" id="">
-        <option value="">none</option>
-        <option value="">highest</option>
-        <option value="">lowest</option>
+      <select name="sorting" id="sorting" onChange={handleSortingChange}>
+        <option value="none">none</option>
+        <option value="highest">highest to lowest</option>
+        <option value="lowest">lowest to highest</option>
       </select>
+
       <p>{changer}</p>
 
-      <NewHabits
-        habits={habits}
-        updatehabit={updatehabit}
-        addhabits={addhabits}
-        // rankingHighToLow={rankingHighToLow}
-      />
+      <NewHabits habits={habits} updatehabit={updatehabit} addhabits={addhabits} />
     </>
   );
 }
